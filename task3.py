b83fa5b645339ca1d24f46f8353a606cbf354dfd
anton2107 <36999432+anton2107@users.noreply.github.com>
2018-03-31 11:03:35 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,15 @@
-
+import scipy.linalg as sl
+import numpy as np
+N=10
+A=np.zeros((N,N))
+B=np.zeros((N))
+i,j=np.indices(A.shape)
+for k in range(int(N)):
+    A[i==j+k]=1
+    B[k]=k
+print('A - matrix:')
+print(A)
+print('B - vector:')
+print(B)
+print('Result vector (answer):')
+print(sl.solve(A,B))
